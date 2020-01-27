71ee63b67309709f13fd0a7e2d52922395ce139c
haxul <s.s.starodubov@yandex.ru>
2020-01-27 22:31:00 +0400

    feat:init

---
+++ src/main/java/com/tcrypto/TcryptoApplication.java
@@ -0,0 +1,13 @@
+package com.tcrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcryptoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tcrypto/TcryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
