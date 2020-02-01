e5c3cbf298ce364fe0a3f622382a087a9a42b771
haxul <s.s.starodubov@yandex.ru>
2020-02-01 21:35:55 +0400

    feat: create tokens

---
+++ src/main/java/com/tcrypto/services/RefreshTokenService.java
@@ -0,0 +1,30 @@
+package com.tcrypto.services;
+
+import com.tcrypto.dao.RefreshTokenDao;
+import com.tcrypto.models.RefreshToken;
+import com.tcrypto.models.impl.TokenAble;
+import com.tcrypto.services.impl.TokenHandleAble;
+import com.tcrypto.utils.StringRandom;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class RefreshTokenService implements TokenHandleAble {
+
+    private final int REFRESH_TOKEN_SIZE = 128;
+    private final RefreshTokenDao refreshTokenDao;
+
+    public RefreshTokenService(RefreshTokenDao refreshTokenDao) {
+        this.refreshTokenDao = refreshTokenDao;
+    }
+
+    @Override
+    public TokenAble createToken() {
+        String randomString = StringRandom.generate(REFRESH_TOKEN_SIZE);
+        RefreshToken token = new RefreshToken(randomString);
+        refreshTokenDao.save(token);
+        return token;
+    }
+}
