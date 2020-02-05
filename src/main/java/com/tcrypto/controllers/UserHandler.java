63cbf8464187f299740d7e29466b5338458f923b
haxul <s.s.starodubov@yandex.ru>
2020-02-05 21:05:09 +0400

    feat: create pagination

--- src/main/java/com/tcrypto/controllers/UserHandler.java
+++ src/main/java/com/tcrypto/controllers/UserHandler.java
@@ -2,6 +2,7 @@ package com.tcrypto.controllers;
 
 import com.tcrypto.dao.CoinDao;
 import com.tcrypto.dao.UserDao;
+import com.tcrypto.models.Coin;
 import com.tcrypto.models.User;
 import com.tcrypto.services.CoinOptions;
 import com.tcrypto.services.UserService;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
@@ -31,17 +36,34 @@ public class UserHandler {
         this.userDao = userDao;
     }
 
-    @PostMapping("/token/{symbol}")
-    public ResponseEntity<String> addCoin(HttpServletRequest request, @PathVariable @Size(max = 10) @Pattern(regexp = "^\\w+$") String symbol) {
+    @PostMapping("/coin/{symbol}")
+    public ResponseEntity<String> addCoin(HttpServletRequest request,
+                                          @PathVariable @Size(max = 10) @Pattern(regexp = "^\\w+$") String symbol) {
         User user = userService.checkAccessToken(request);
         userService.toggleCoin(user, symbol, CoinOptions.ADD);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping("/token/{symbol}")
-    public ResponseEntity<String> removeCoin(HttpServletRequest request, @PathVariable @Size(max = 10) @Pattern(regexp = "^\\w+$") String symbol) {
+    @DeleteMapping("/coin/{symbol}")
+    public ResponseEntity<String> removeCoin(HttpServletRequest request,
+                                             @PathVariable @Size(max = 10) @Pattern(regexp = "^\\w+$") String symbol) {
         User user = userService.checkAccessToken(request);
         userService.toggleCoin(user, symbol, CoinOptions.REMOVE);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/coin")
+    public ResponseEntity<Set<Coin>> getUserCoins(HttpServletRequest request,
+                                                  @RequestParam long skip,
+                                                  @RequestParam long limit) {
+        User user = userService.checkAccessToken(request);
+        Set<Coin> coins = user.getCoins()
+                .stream()
+                .sorted(Comparator.comparingInt(Coin::getId))
+                .skip(skip)
+                .limit(limit)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+        return new ResponseEntity<>(coins, HttpStatus.OK);
+
     }
 }
